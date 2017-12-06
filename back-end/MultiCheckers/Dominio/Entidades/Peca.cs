67f59489994b03d56bb9e6b02ddcdc70e4dc6c88
Victor Herzog <victor.damke@cwi.com.br>
2017-12-06 17:26:10 -0200

    wip(back-end): criando entidades peca e tabuleiro.

--- back-end/MultiCheckers/Dominio/Entidades/Peca.cs
+++ back-end/MultiCheckers/Dominio/Entidades/Peca.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using Dominio.Entidades;
 
 namespace Dominio
 {
     public class Peca
     {
+        private Peca()
+        { }
+
+        public Peca(System.Drawing.Point posicaoInicial, Cor cor)
+        {
+            this.PosicaoInicial = posicaoInicial;
+            this.Cor = cor;
+        }
+
+        public Point PosicaoInicial { get; private set; }
+
+        public Cor Cor { get; private set; }
     }
 }
