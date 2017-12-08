6e73c422b4906f137feda0df6c6094f620c4772c
Bruno Hoffmann <bruno.siqueira.hoffmann@gmail.com>
2017-12-08 15:26:13 -0200

    refactor(front-end): alterando jogoService e jogoCtrl

--- front-end/jogo/jogo.controller.js
+++ front-end/jogo/jogo.controller.js
@@ -1,12 +1,24 @@
 angular.module('app')
-.controller('JogoCtrl', function($scope, jogoService, $routeParams) {
+.controller('JogoCtrl', function($scope, jogoService, $routeParams, $interval) {
+    carregarJogo();
+    $scope.corDoJogador = 0;
     $scope.urlSala = $routeParams.urlSala;
 
     function carregarJogo(){
-        jogoService.buscarTabuleiro().then(response => {
-            $scope.pecas = response.data.dados.Pecas;
+        jogoService.buscarJogo().then(response => {
+            $scope.pecas = response.data.dados.tabuleiro.Pecas;
+            $scope.corJogando = response.data.dados.cor;
         });
     }
+    $interval(carregarJogo, 1000);
+    $scope.jogar = function(){
+        if($scope.corDoJogador != $scope.corJogando){
+            alert("não é sua vez");
+            return
+        }
+        /*TO-DO: implementar jogo*/
+    }
+
     /*
     $scope.gerarPartida = function(){
         $scope.url = "aaaaaaaa";
@@ -14,6 +26,6 @@ angular.module('app')
         $scope.gerado = !!$scope.url;
     }*/
     $scope.copiar = function(url){
-        
+
     }
 });
