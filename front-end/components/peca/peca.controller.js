f29286f81d83d3fe9f01cb255327b162bb347782
francisco.mossi <kikomossi@gmail.com>
2017-12-08 19:17:23 -0200

    feat(jogo): adicionando movimentação de peças

--- front-end/components/peca/peca.controller.js
+++ front-end/components/peca/peca.controller.js
@@ -5,13 +5,32 @@ angular.module('app')
         
         $scope.setPeca = function(peca){
             $scope.peca = peca;
-            $scope.top = 12.5 *  $scope.peca.PosicaoAtual.Y;
-            $scope.left = 12.5 *  $scope.peca.PosicaoAtual.X;
+            $scope.selecionada = false;
+            var posicao = $scope.peca.PosicaoAtual.split(", ");
+            $scope.peca.x = 12.5 * parseInt(posicao[0]) + 1.2;
+            $scope.peca.y = 12.5 * parseInt(posicao[1]) + 1.2;
+            
+            let movimentosString = [];
+            $scope.peca.PosicoesPossiveis
+                                .forEach(
+                                    n => movimentosString.push(n.split(", "))
+                                );
+
+            $scope.movimentos = [];
+            movimentosString.forEach(
+                n => $scope.movimentos.push({"x": parseInt(n[0]), 
+                             "y": parseInt(n[1])}
+                            ));
         }
 
+        $scope.mostrarMovimentos = function(){
+            $scope.selecionada = true;
+        }
 
         $scope.moverPeca = function(posicao){
-            $scope.peca.PosicaoAtual.X = posicao
+            $scope.peca.x = 12.5 * posicao.x + 1.2;
+            $scope.peca.y = 12.5 * posicao.y + 1.2;
+            $scope.selecionada = false;
         }
     }
 );    
--- front-end/jogo/jogo.controller.js
+++ front-end/jogo/jogo.controller.js
@@ -13,6 +13,18 @@ angular.module('app')
 
         $scope.gerado = !!$scope.url;
     }*/
+
+
+    $scope.peca = {
+        "PosicaoAtual": "5, 3",
+        "Cor": 0,
+        "PosicoesPossiveis": [
+          "6, 4",
+          "4, 4"
+        ],
+        "IsDama": false
+      };
+
     $scope.copiar = function(url){
         
     }
