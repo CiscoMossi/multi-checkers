28df3fcb5af8829e244312ccc2ef8b7803c2bf43
Francisco Wolfarth Mossi <francisco.mossi@cwi.com.br>
2017-12-13 15:37:05 -0200

    feat(home): adicionando leaderboard.

--- front-end/home/home.controller.js
+++ front-end/home/home.controller.js
@@ -1,39 +1,46 @@
 angular.module('app')
-.controller('HomeCtrl', function($scope, jogoService, $location, $sessionStorage, usuarioService, $localStorage) {
-    $scope.gerarPartida = function(){
-        $scope.url = "";
-        jogoService.gerarUrl();
-    }
+    .controller('HomeCtrl', function ($scope, jogoService, $location, $sessionStorage, usuarioService, $localStorage, historicoService) {
+        $scope.gerarPartida = function () {
+            $scope.url = "";
+            jogoService.gerarUrl();
+        }
 
-    $scope.usuarioLogado = $localStorage.usuarioLogado;
-    
-    function buscar(id) {
-        historicoService.buscar(id)
-            .then(function (response) {
-                $scope.usuarioLogado.pontos = response.data.pontos;
-                $scope.usuarioLogado.partidas = response.data.partidas;
-                $scope.usuarioLogado.vitorias = response.data.vitorias;
-                $scope.usuarioLogado.pecasDestruidas = response.data.pecasDestruidas;
-            })
-    }
-    buscar(usuarioLogado.Id);
+        $scope.usuarioLogado = $localStorage.usuarioLogado;
 
-    $scope.$on('criarSala', function(event, urlSala){
-        $scope.url = urlSala;
-        $location.path(`sala/${$scope.url}`);
-        $scope.gerado = !!$scope.url;
-        $scope.$apply();
+        function buscar(id) {
+            historicoService.buscar(id)
+                .then(function (response) {
+                    if (response.data == 0) {
+                        $scope.usuarioLogado.pontos = response.data;
+                        $scope.usuarioLogado.partidas = response.data;
+                        $scope.usuarioLogado.vitorias = response.data;
+                        $scope.usuarioLogado.pecasDestruidas = response.data;
+                    } else {
+                        $scope.usuarioLogado.pontos = response.data.pontos;
+                        $scope.usuarioLogado.partidas = response.data.partidas;
+                        $scope.usuarioLogado.vitorias = response.data.vitorias;
+                        $scope.usuarioLogado.pecasDestruidas = response.data.pecasDestruidas;
+                    }
+                })
+        }
+        buscar($scope.usuarioLogado.Id);
+
+        $scope.$on('criarSala', function (event, urlSala) {
+            $scope.url = urlSala;
+            $location.path(`sala/${$scope.url}`);
+            $scope.gerado = !!$scope.url;
+            $scope.$apply();
+        });
+        $scope.$on('isConnect', function (event, connect) {
+            debugger;
+            $sessionStorage.connect = connect;
+            $scope.$apply();
+        });
+
+        $scope.copiar = function () {
+            let texto = document.getElementById("url");
+            texto.select();
+            document.execCommand("Copy");
+        }
+
     });
-    $scope.$on('isConnect', function(event, connect){
-        debugger;
-        $sessionStorage.connect = connect;
-        $scope.$apply();
-    });
-
-    $scope.copiar = function(){
-        let texto = document.getElementById("url");
-        texto.select();
-        document.execCommand("Copy");
-    }
-
-});
