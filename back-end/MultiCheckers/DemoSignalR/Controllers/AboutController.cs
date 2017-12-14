18234b4bf049f234e2dca9926b77bd82ca5e15c4
v-herzog <v.herzog@hotmail.com>
2017-12-14 17:45:03 -0200

    feat(about): adicionando about controller.

---
+++ back-end/MultiCheckers/DemoSignalR/Controllers/AboutController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+
+namespace MultiCheckers.Api.Controllers
+{
+    [RoutePrefix("api/about")]
+    public class AboutController : ApiController
+    {
+        static string ApiVersion;
+
+        static AboutController()
+        {
+            ApiVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        [HttpGet]
+        [Route("Version")]
+        public IHttpActionResult GetVersion()
+        {
+            return Ok(ApiVersion);
+        }
+
+        [HttpGet]
+        [Route("ServerName")]
+        public IHttpActionResult GetServerName()
+        {
+            return Ok(Environment.MachineName);
+        }
+    }
+}
