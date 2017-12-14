6b93ddd4cbf626385b5605bac4cc5eefaac84f1e
Bruno Hoffmann <bruno.siqueira.hoffmann@gmail.com>
2017-12-14 18:18:55 -0200

    Merge

--- front-end/jogo/jogo.controller.js
+++ front-end/jogo/jogo.controller.js
@@ -46,41 +46,63 @@ angular.module('app')
                 $scope.corJogando = parseInt(partida.Tabuleiro.CorTurnoAtual);
                 $scope.jogadorBrancas = partida.JogadorBrancas
                 $scope.jogadorPretas = partida.JogadorPretas
-
-            if($sessionStorage.usuarioCor == 0){
-                $scope.quantidadeDePecasUsuario = $scope.brancas.length;
-                $scope.quantidadeDePecasOponente = $scope.pretas.length;
-            }else{
-                $scope.quantidadeDePecasUsuario = $scope.pretas.length;
-                $scope.quantidadeDePecasOponente = $scope.brancas.length;
-            }
-            $scope.$apply();
-        });
-        $scope.$on('fimJogo', function (event, mensagem) {
-            if(mensagem == "BRANCA"){
-                $scope.corGanhadora = 0;
-            }else{
-                $scope.corGanhadora = 1;
-            }
-            if(acabouPartida == false){
-                if($scope.corGanhadora == $sessionStorage.usuarioCor){
-                    jogoService.finalizaJogo({"LoginUsuario": authService.getUsuario().Login, "Venceu": true, "PecasRestantes":  $scope.quantidadeDePecasUsuario, "PecasEliminadas": 12 - $scope.quantidadeDePecasOponente});
-                }else if($scope.corGanhadora != $sessionStorage.usuarioCor){
-                    jogoService.finalizaJogo({"LoginUsuario": authService.getUsuario().Login, "Venceu": false, "PecasRestantes":  $scope.quantidadeDePecasUsuario, "PecasEliminadas": 12 - $scope.quantidadeDePecasOponente});
+                /*
+                if (!!$scope.jogadorBrancas) {
+                    historicoService.buscar($scope.jogadorBrancas.Id)
+                        .then(function (response) {
+                            if (response.data === 0) {
+                                $scope.pontosBrancas = response.data;
+                            } else {
+                                $scope.pontosBrancas = response.data.pontos;
+                            }
+                        })
                 }
-                acabouPartida = true;
-            }
-            modal.style.display = "flex";
-            modal.style.justifyContent = "center";
-            $scope.corGanhadora;
-            $scope.$apply();
-        });
-    
-        $scope.mostrarMovimentos = function(peca,cor){
-            if($scope.corJogando == peca.Cor && 
-                peca.PosicoesPossiveis.length != 0 &&
-                $scope.corJogando == $sessionStorage.usuarioCor)
-                $scope.selecionada = peca;
+
+                if (!!$scope.jogadorPretas) {
+                    historicoService.buscar($scope.jogadorPretas.Id)
+                        .then(function (response) {
+                            if (response.data === 0) {
+                                $scope.pontosPretas = response.data;
+                            } else {
+                                $scope.pontosPretas = response.data.pontos;
+                            }
+                        })
+                }
+                */
+                if ($sessionStorage.usuarioCor == 0) {
+                    $scope.quantidadeDePecasUsuario = $scope.brancas.length;
+                    $scope.quantidadeDePecasOponente = $scope.pretas.length;
+                } else {
+                    $scope.quantidadeDePecasUsuario = $scope.pretas.length;
+                    $scope.quantidadeDePecasOponente = $scope.brancas.length;
+                }
+                $scope.$apply();
+            });
+            $scope.$on('fimJogo', function (event, mensagem) {
+                if (mensagem == "BRANCA") {
+                    $scope.corGanhadora = 0;
+                } else {
+                    $scope.corGanhadora = 1;
+                }
+                if (acabouPartida == false) {
+                    if ($scope.corGanhadora == $sessionStorage.usuarioCor) {
+                        jogoService.finalizaJogo({ "LoginUsuario": authService.getUsuario().Login, "Venceu": true, "PecasRestantes": $scope.quantidadeDePecasUsuario, "PecasEliminadas": 12 - $scope.quantidadeDePecasOponente });
+                    } else if ($scope.corGanhadora != $sessionStorage.usuarioCor) {
+                        jogoService.finalizaJogo({ "LoginUsuario": authService.getUsuario().Login, "Venceu": false, "PecasRestantes": $scope.quantidadeDePecasUsuario, "PecasEliminadas": 12 - $scope.quantidadeDePecasOponente });
+                    }
+                    acabouPartida = true;
+                }
+                modal.style.display = "flex";
+                modal.style.justifyContent = "center";
+                $scope.corGanhadora;
+                $scope.$apply();
+            });
+
+            $scope.mostrarMovimentos = function (peca, cor) {
+                if ($scope.corJogando == peca.Cor &&
+                    peca.PosicoesPossiveis.length != 0 &&
+                    $scope.corJogando == $sessionStorage.usuarioCor)
+                    $scope.selecionada = peca;
             }
         }
 
@@ -90,7 +112,7 @@ angular.module('app')
 
         $scope.mostrarUrl = function () {
             $scope.mostrar = !$scope.mostrar;
-            $scope.url = "localhost:8081/#!/sala/"+$routeParams.urlSala;
+            $scope.url = "localhost:8081/#!/sala/" + $routeParams.urlSala;
         }
 
         $scope.copiar = function () {
