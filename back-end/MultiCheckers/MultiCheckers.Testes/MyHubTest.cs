aa89d45097170aaf26d59848fc33b75f61f761f4
Bruno Hoffmann <bruno.siqueira.hoffmann@gmail.com>
2017-12-16 19:14:20 -0200

    test(SignalR): adicionando testes

---
+++ back-end/MultiCheckers/MultiCheckers.Testes/MyHubTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using MultiCheckers.Api;
+using Microsoft.AspNet.SignalR.Hubs;
+using Moq;
+using System.Dynamic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiCheckers.Testes
+{
+    [TestClass]
+    public class MyHubTest
+    {
+        public interface IClientContract
+        {
+            string criarSala(string name);
+            void isConnect(bool isConnect);
+            void infoJogador(string info);
+        }
+        [TestMethod]
+        public void CriarSala()
+        {
+            var hub = new MyHub();
+            var mockClients = new Mock<IHubCallerConnectionContext<dynamic>>();
+            var all = new Mock<IClientContract>();
+            hub.Clients = mockClients.Object;
+            all.Setup(m => m.criarSala(It.IsAny<string>())).Verifiable();
+            mockClients.Setup(m => m.Caller).Returns(all.Object);
+            hub.CriarSala();
+            all.VerifyAll();
+        }
+        [TestMethod]
+        public void IsConnect()
+        {
+            var hub = new MyHub();
+            var mockClients = new Mock<IHubCallerConnectionContext<dynamic>>();
+            var all = new Mock<IClientContract>();
+            hub.Clients = mockClients.Object;
+            all.Setup(m => m.isConnect(It.IsAny<bool>())).Verifiable();
+            mockClients.Setup(m => m.Caller).Returns(all.Object);
+            hub.OnConnected();
+            all.VerifyAll();
+        }
+        [TestMethod]
+        public void Consultar_Partida_Inexistente()
+        {
+            var hub = new MyHub();
+            var mockClients = new Mock<IHubCallerConnectionContext<dynamic>>();
+            var all = new Mock<IClientContract>();
+            hub.Clients = mockClients.Object;
+            all.Setup(m => m.infoJogador(It.IsAny<string>())).Verifiable();
+            mockClients.Setup(m => m.Caller).Returns(all.Object);
+            hub.Consultar("");
+            all.Verify();
+        }
+    }
+}
