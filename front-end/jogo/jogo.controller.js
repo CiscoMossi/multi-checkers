51d535040d2197cf647b746fdf5aa4dcf12d14be
francisco.mossi <kikomossi@gmail.com>
2017-12-17 15:59:46 -0200

    refactor(jogo): adicionando tag que indica se usuario é espectador.

--- front-end/jogo/jogo.controller.js
+++ front-end/jogo/jogo.controller.js
@@ -1,10 +1,11 @@
 angular.module('app')
-    .controller('JogoCtrl', function ($scope, authService, $location, $timeout, jogoService, $routeParams, $interval, pecaService, $rootScope, $sessionStorage, historicoService) {
+    .controller('JogoCtrl', function ($scope, authService, $location, $timeout, jogoService, $routeParams, $interval, pecaService, $rootScope, $sessionStorage, historicoService, $localStorage) {
         var vitoria = new Audio('../sounds/vitoria.mp3');
         var movimentacao = new Audio('../sounds/movimentacao_peca.mp3');
         //var virandoJogador = new Audio('../sounds/virando_jogador.mp3');
         var virandoJogador = new Audio('../sounds/v-jogador.mp3');
-        var acabouPartida = false;
+        $scope.usuarioLogado = $localStorage.usuarioLogado;
+        
         if ($routeParams.urlSala == null || $routeParams.urlSala == undefined) {
             $location.path('/home');
         }
