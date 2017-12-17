e47ab49dc94feefb02e9c488cb66f6d03a09d5ea
francisco.mossi <kikomossi@gmail.com>
2017-12-17 19:27:19 -0200

    feat(test): adicionando testes de controller.

---
+++ back-end/MultiCheckers/MultiCheckers.Testes/HistoricoControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiCheckers.Api.Controllers;
+using Infra;
+using System.Web.Http.Results;
+
+namespace MultiCheckers.Testes
+{
+    [TestClass]
+    public class HistoricoControllerTest
+    {
+        [TestMethod]
+        public void Obter_Pontos_Usuario()
+        {
+            //Arrange
+            MultiCheckersContext contexto = new MultiCheckersContext();
+            HistoricoController controller = new HistoricoController(contexto);
+            //Act
+            var result = controller.ObterPontosDoUsuario(1);
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public void Obter_Historico()
+        {
+            //Arrange
+            MultiCheckersContext contexto = new MultiCheckersContext();
+            HistoricoController controller = new HistoricoController(contexto);
+            //Act
+            var result = controller.Listar(1);
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public void Nao_Obter_Historico()
+        {
+            //Arrange
+            MultiCheckersContext contexto = new MultiCheckersContext();
+            HistoricoController controller = new HistoricoController(contexto);
+            //Act
+            var result = controller.Listar(214748364);
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+    }
+}
---
+++ back-end/MultiCheckers/MultiCheckers.Testes/UsuarioControllerTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiCheckers.Api.Controllers;
+using Infra;
+using System.Web.Http.Results;
+using Dominio;
+using MultiCheckers.Api.Models;
+
+namespace MultiCheckers.Testes
+{
+    [TestClass]
+    public class UsuarioControllerTest
+    {
+        [TestMethod]
+        public void Obter_Usuario()
+        {
+            //Arrange
+            MultiCheckersContext contexto = new MultiCheckersContext();
+            UsuarioController controller = new UsuarioController(contexto);
+            //Act
+            var result = controller.ObterUsuario(1);
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public void Nao_Obter_Usuario_Inexistente()
+        {
+            //Arrange
+            MultiCheckersContext contexto = new MultiCheckersContext();
+            UsuarioController controller = new UsuarioController(contexto);
+            //Act
+            var result = controller.ObterUsuario(0);
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+
+        [TestMethod]
+        public void Nao_Criar_Usuario()
+        {
+            //Arrange
+            MultiCheckersContext contexto = new MultiCheckersContext();
+            UsuarioController controller = new UsuarioController(contexto);
+            UsuarioModel model = new UsuarioModel();
+            //Act
+            var result = controller.AdicionarUsuario(model);
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+
+        [TestMethod]
+        public void Criar_Usuario()
+        {
+            //Arrange
+            MultiCheckersContext contexto = new MultiCheckersContext();
+            UsuarioController controller = new UsuarioController(contexto);
+            UsuarioModel model = new UsuarioModel();
+            model.Email = "@@@";
+            model.Login = "Login";
+            model.Senha = "senha";
+            //Act
+            var result = controller.AdicionarUsuario(model);
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public void Nao_Obter_Usuario_Logado()
+        {
+            //Arrange
+            MultiCheckersContext contexto = new MultiCheckersContext();
+            UsuarioController controller = new UsuarioController(contexto);
+            //Act
+            var result = controller.Obter();
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+    }
+}
