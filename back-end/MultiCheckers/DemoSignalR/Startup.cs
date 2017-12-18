a78c809e538abfac5ad1ae77730d80f3b401d137
Bruno Hoffmann <bruno.siqueira.hoffmann@gmail.com>
2017-12-17 23:26:17 -0200

    refactor(back-end): removendo comentários

--- back-end/MultiCheckers/DemoSignalR/Startup.cs
+++ back-end/MultiCheckers/DemoSignalR/Startup.cs
@@ -9,16 +9,8 @@ namespace MultiCheckers.Api
     {
         public void Configuration(IAppBuilder app)
         {
-            // Isso aqui é perigoso, não façam isso em projetos de verdade :)
             app.UseCors(CorsOptions.AllowAll);
             app.MapSignalR();
-            
-            //// Branch the pipeline here for requests that start with "/signalr"
-            //app.Map("/signalr", map =>
-            //{
-            //    map.UseCors(CorsOptions.AllowAll);
-            //    map.RunSignalR();
-            //});
         }
     }
 }
